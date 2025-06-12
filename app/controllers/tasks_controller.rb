dd8183c95d41ea97b0cbde2e6e15de74a857dcdc
anallbani <anxhoelnallbani@gmail.com>
2025-06-12 03:01:31 +0200

    Add Tailwind CSS and update views

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: %i[show edit update destroy]
+  before_action :set_task, only: %i[show edit update destroy toggle]
 
   def index
     @tasks = Task.all
@@ -17,11 +17,10 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
-
     if @task.save
       redirect_to @task, notice: "Task was successfully created."
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -29,7 +28,7 @@ class TasksController < ApplicationController
     if @task.update(task_params)
       redirect_to @task, notice: "Task was successfully updated."
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
@@ -38,61 +37,20 @@ class TasksController < ApplicationController
     redirect_to tasks_url, notice: "Task was successfully destroyed."
   end
 
-  private
-
-  def set_task
-    @task = Task.find(params[:id])
-  end
-
-
-  def task_params
-    params.require(:task).permit(:title, :details, :completed)
-  end
-
   def toggle
-    @task = Task.find(params[:id])
     @task.update(completed: !@task.completed)
     respond_to do |format|
       format.json { render json: { completed: @task.completed } }
     end
   end
 
-  def new
-    @task = Task.new
-  end
-
-  def create
-    @task = Task.new(task_params)
-    if @task.save
-      redirect_to @task
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
-
-  def edit
-    @task = Task.find(params[:id])
-  end
-
-  def update
-    @task = Task.find(params[:id])
-    if @task.update(task_params)
-      redirect_to @task
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @task = Task.find(params[:id])
-    @task.destroy
-    redirect_to tasks_path
-  end
-
   private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
   end
-
 end
